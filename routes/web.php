d544ca01fc9d2b12491091b66ccf1fca3fdd4ead
Tetang-Jerry <juanjerry120@gmail.com>
2024-07-18 00:05:29 +0200

    restored everything to default

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Amin\AdminController;
 
 
-Route::get('/', [UserController::class, 'index']);
+Route::get('/', [AdminController::class, 'adminView'])->name('admin');
+Route::get('/user', [UserController::class, 'userView'])->name('user');
+
+
+
