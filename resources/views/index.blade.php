857612d02b2c40cbf44154bc7d0f3756e8dc6462
Tetang-Jerry <juanjerry120@gmail.com>
2024-07-17 12:00:14 +0200

    changed

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <section>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia recusandae magni vel nostrum consectetur neque architecto ex quas sint voluptatem nulla amet nihil autem, velit natus nobis? Accusamus, et eaque totam repellendus, molestias ex similique, amet dolore unde quidem obcaecati deleniti eius dolores. Vero, ex aperiam? Voluptas, sed alias.
+            </p>
+        </section>
+    </main>
+</body>
+</html>
