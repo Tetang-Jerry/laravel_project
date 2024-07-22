771c9ac30ac09e5a88982cfa41ce2d0b69436d2f
Tetang-Jerry <juanjerry120@gmail.com>
2024-07-22 10:56:05 +0200

    routes registers grouper

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'home'])->name('home');
 Route::get('/login', [loginController::class, 'loginView'])->name('loginView');
-Route::get('/register', [registerController::class, 'registerView'])->name('registerView');
-Route::get('/register1', [registerController::class, 'registerView_1'])->name('registerView1');
-Route::get('/register_2', [registerController::class, 'registerView_2'])->name('registerView2');
-Route::get('/modal', [registerController::class, 'modalView'])->name('modalView');
-Route::get('/code', [registerController::class, 'codeView'])->name('codeView');
 Route::get('/passwordView', [loginController::class, 'passwordForgottenView'])->name('passView');
 Route::get('/modalVerif', [loginController::class, 'modalVerifView'])->name('modalVerifView');
 Route::get('/codeVerif', [loginController::class, 'codeVerifView'])->name('codeVerifView');
 Route::get('/dashboard', [LayoutController::class, 'layoutdash'])->name('layoutdashView');
+
+
+
+    Route::prefix('register')->group( function () {
+        Route::get('/register', [registerController::class, 'registerView'])->name('registerView');
+        Route::get('/register1', [registerController::class, 'registerView_1'])->name('registerView1');
+        Route::get('/register_2', [registerController::class, 'registerView_2'])->name('registerView2');
+        Route::get('/modal', [registerController::class, 'modalView'])->name('modalView');
+        Route::get('/code', [registerController::class, 'codeView'])->name('codeView');
+    });
+
+
 
