d23fc750955fe1975a06d14343e5005fe929f709
Tetang-Jerry <juanjerry120@gmail.com>
2024-07-26 13:15:52 +0200

    update

--- app/Models/Alpha_transit_user.php
+++ app/Models/Alpha_transit_user.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Alpha_transit_user extends Model
 {
@@ -13,4 +14,10 @@ class Alpha_transit_user extends Model
     protected $fillable = [
         'nom', 'prenom', 'email', 'username', 'password', 'numero', 'code', 'passwordVerify', 'codeVerify', 'token', 'numCompte'
     ];
+}
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    // ...
 }
