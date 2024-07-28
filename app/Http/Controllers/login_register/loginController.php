c57a574ff2783fe5adb95e6d4856902841d6c4b1
Tetang-Jerry <juanjerry120@gmail.com>
2024-07-28 13:01:50 +0200

    session updated in the logout fonction

--- app/Http/Controllers/login_register/loginController.php
+++ app/Http/Controllers/login_register/loginController.php
@@ -47,10 +47,12 @@ class loginController extends Controller
     public function logoutUser()
     {
         if (Auth::guard('user_auth')->check()) {
-            Auth::guard('user_auth')->logout();
             $user =Auth::guard('user_auth')->user();
+            $user->update(['session' => 0]);
+            Auth::guard('user_auth')->logout();
+
         }
-        $user->update(['session' => 0]);
+
         return redirect()->route('loginView');
     }
 }
