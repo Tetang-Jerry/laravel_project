4a9653fffe6264d8a1fa93c7b02e62f4986ae112
Tetang-Jerry <juanjerry120@gmail.com>
2024-07-29 12:41:55 +0200

    moad admin

--- resources/views/layout/topbar.blade.php
+++ resources/views/layout/topbar.blade.php
@@ -6,6 +6,7 @@
     <title>layout</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="{{asset('css/anime.css')}}">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @vite('resources/css/app.css')
     @yield('cssImport')
 </head>
@@ -79,7 +80,28 @@
 
 
 
+<script >
+    @yield('script')
 
+</script>
+<script>
+        @if(session('success'))
+            Swal.fire({
+                title: 'Success!',
+                text: "{{ session('success') }}",
+                icon: 'success',
+                confirmButtonText: 'OK'
+            });
+        @endif
 
+        @if(session('error'))
+            Swal.fire({
+                title: 'Error!',
+                text: "{{ session('error') }}",
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        @endif
+    </script>
 </body>
 </html>
