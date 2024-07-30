11b8b809323481cca96bc2a058002c31dafcec88
alphaomegacorporate <berlinkoueni25@gmail.com>
2024-07-31 00:47:43 +0100

    mise a jour de la navbar de l'administrateur

--- resources/views/layout/topbar.blade.php
+++ resources/views/layout/topbar.blade.php
@@ -6,25 +6,24 @@
     <title>layout</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="{{asset('css/anime.css')}}">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @vite('resources/css/app.css')
     @yield('cssImport')
 </head>
 <body class="bg-backgrounddashboard flex-col flex ">
 <!-- header -->
 
-<header class=" fixed w-full  mx-auto  py-3 px-16 shadow-md bg-white z-20 top-0  ">
-    <!-- flex container  -->
-    <div class="flex items-center justify-between ">
+<header class="fixed w-full mx-auto py-3 px-16 shadow-md bg-white z-20 top-0">
+    <!-- flex container -->
+    <div class="flex items-center justify-between">
         <!--logo -->
         <div class="pt-2">
-            <img src="/images/watermark_preview_image20240717-1-urrith-removebg-preview.png" class="w-[120px] " alt="Alpha transit Logo">
+            <img src="/images/watermark_preview_image20240717-1-urrith-removebg-preview.png" class="w-[120px]" alt="Alpha transit Logo">
         </div>
 
         <!--menu-items -->
         <div class="hidden md:flex space-x-[20px]">
             <a href="#" class="hover:text-customBlue text-[16px]">Home</a>
-            <a href="#" class="hover:text-customBlue text-[16px]" >Dashboard</a>
+            <a href="#" class="hover:text-customBlue text-[16px]">Dashboard</a>
             <a href="#" class="hover:text-customBlue text-[16px]">Services</a>
             <a href="#" class="hover:text-customBlue text-[16px]">History</a>
             <a href="#" class="hover:text-customBlue text-[16px]">Help</a>
@@ -32,19 +31,35 @@
 
         <div class="flex items-center justify-between space-x-[40px]">
             <!--icons -->
-
             <div class="flex space-x-[20px] ml-[50px]">
                 <i class="fa-solid fa-gear text-customBlue text-[20px]"></i>
                 <i class="fa-regular fa-bell text-customBlue text-[20px]"></i>
             </div>
 
             <!-- profile -->
-            <div class="flex space-x-[20px] items-center ">
-                <p>Ashley</p>
-                <img src="/images/profile.jpg" class="h-[40px] w-[40px] rounded-full " alt="profile-picture">
+            <div class="relative cursor-pointer group pl-4 border-l border-gray-400 flex space-x-[20px] items-center">
+                <p class="font-semibold">berlin</p>
+                <ion-icon name="person-circle-outline" class="text-4xl"></ion-icon>
+
+
+                <!-- Dropdown menu -->
+                <div class="absolute hidden -right-9 top-10 group-hover:block first-letter: z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                        <div>Account number</div>
+                        <div class="font-medium truncate">admin 1</div>
+                    </div>
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit profile</a>
+                        </li>
+                    </ul>
+                    <div class="py-2">
+                        <a href="{{route('logoutUser')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                    </div>
+                </div>
+
             </div>
         </div>
-
 
     </div>
 
@@ -79,29 +94,9 @@
 
 
 
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
-<script >
-    @yield('script')
 
-</script>
-<script>
-        @if(session('success'))
-            Swal.fire({
-                title: 'Success!',
-                text: "{{ session('success') }}",
-                icon: 'success',
-                confirmButtonText: 'OK'
-            });
-        @endif
-
-        @if(session('error'))
-            Swal.fire({
-                title: 'Error!',
-                text: "{{ session('error') }}",
-                icon: 'error',
-                confirmButtonText: 'OK'
-            });
-        @endif
-    </script>
 </body>
 </html>
